2dbf738c98e7cdf7e6807a8711877326e7a7a68e
Kevin Cioch <kevin.cioch@gmail.com>
2023-10-14 01:18:01 +0200

    Extract ErrorMsg component

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { createIssueSchema } from "../../models/validationSchema";
+import { createIssueSchema } from "../../../models/validationSchema";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
